6d199fc9037f1dcf53c2faf48aa50d678ec1b77d
Anna Riehle <riehlea@gmail.com>
2018-06-10 13:38:22 -0400

    Make some adjustments

--- app.py
+++ app.py
@@ -9,15 +9,18 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-from sqlalchemy import inspect
 
 app = Flask(__name__)
 
 engine = create_engine("sqlite:///db/belly_button_biodiversity.sqlite")
-session = Session(engine)
-inspector = inspect(engine)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
+
+OTU = Base.classes.otu
+samp_meta = Base.classes.samples_metadata
+Samples = Base.classes.samples
+
+session = Session(engine)
 
 @app.route("/")
 def home():
@@ -26,18 +29,14 @@ def home():
 
 @app.route("/names")
 def sample_names():
-    results = inspector.get_columns('samples')
+    results = session.query(samp_meta.SAMPLEID).all()
     names = []
-    i = 1
-    while i < len(results):
-        names.append(results[i]['name'])
-        i += 1
-    
+    for name in results:
+        names.append("BB_" + str(name[0]))  
     return jsonify(names)
 
 @app.route("/otu")
 def get_otu_descriptions():
-    OTU = Base.classes.otu
     results = session.query(OTU.lowest_taxonomic_unit_found).all()
     otu_descriptions = []
     i = 0
@@ -50,7 +49,6 @@ def get_otu_descriptions():
 @app.route("/metadata/<sample>")
 def get_sample_metadata(sample):
     sample = int(sample[3:])
-    samp_meta = Base.classes.samples_metadata
     results = session.query(samp_meta.AGE, samp_meta.BBTYPE, samp_meta.ETHNICITY, samp_meta.GENDER, samp_meta.LOCATION, samp_meta.SAMPLEID).filter(samp_meta.SAMPLEID == sample)
     results_dict = {"AGE": results[0][0], "BBTYPE": results[0][1], "ETHNICITY": results[0][2], "GENDER": results[0][3], "LOCATION": results[0][4], "SAMPLEID": results[0][5]}
     return jsonify(results_dict)
@@ -58,14 +56,12 @@ def get_sample_metadata(sample):
 @app.route("/wfreq/<sample>")
 def get_washing_freq(sample):
     sample = int(sample[3:])
-    samp_meta = Base.classes.samples_metadata
     results = session.query(samp_meta.WFREQ).filter(samp_meta.SAMPLEID == sample)    
     washing_freq = int(results[0][0])
     return jsonify(washing_freq)
     
 @app.route("/samples/<sample>")
 def get_sample_values(sample):
-    Samples = Base.classes.samples
     results = session.query(Samples.otu_id, getattr(Samples, sample)).order_by(getattr(Samples, sample).desc())
     otu_ids = []
     sample_values = []
